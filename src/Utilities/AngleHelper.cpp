15eba66ecf0a5de909d24d9029f5304dce281d81
wqxingchen <wqxingchen@qq.com>
2019-12-06 22:15:30 +0800

    修改编译错误

--- src/Utilities/AngleHelper.cpp
+++ src/Utilities/AngleHelper.cpp
@@ -1,4 +1,4 @@
-#include "stdafx.h"
+ï»¿#include "stdafx.h"
 #include "AngleHelper.h"
 #include "GeometryHelper.h"
 
@@ -27,13 +27,13 @@ CStringW AngleHelper::FormatAngle(double angle, tkAngleFormat angleType, int pre
 
 	if (angleType == tkAngleFormat::afDegrees)
 	{
-		s.Format(format + L"°", angle);		//L"%.1f°"
+		s.Format(format + L"Â°", angle);		//L"%.1fÂ°"
 		return s;
 	}
 
 	// minutes
 	format = "%02d";	// ignore fractional part for minutes and seconds
-	CStringW degreeFormat = reducedBearing ? "%02d° " : "%d° ";
+	CStringW degreeFormat = reducedBearing ? "%02d?" : "%d?";
 
 	int degrees = (int)floor(angle);
 	double fMinutes = (angle - degrees) * 60.0;
